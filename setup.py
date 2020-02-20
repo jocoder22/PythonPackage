2f2755ead27671506b91c4e9cb880917498048d3
jocoder22 <okigbookey@gmail.com>
2020-02-19 19:57:58 -0500

    Add setup arguments

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name = 'catanalysis',
+      version = '0.1.0',
+      description = 'Categorical Analysis',
+      author='Okechukwu Okigbo',
+      author_email='okigbookey@gmail.com',
+      packages=['catanalysis'], 
+      install_requires=[], 
+      zip_safe = False)
